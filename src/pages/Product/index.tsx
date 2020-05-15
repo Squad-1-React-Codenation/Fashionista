99f22663d526a7fb34faba24ac30e0f6d97ab881
Marcelo Reis <oi@marcelreis.dev>
2020-05-14 21:16:23 -0300

    style: prettier format

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Product = () => {
   return (
     <div>
       <h1>Vestido transpassado</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
