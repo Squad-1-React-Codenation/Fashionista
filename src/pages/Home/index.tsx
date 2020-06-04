c1f1b25c63d457f604dd8f067add9249801fb3a5
Carlos Elói dos Santos Prado <carloseloi.prado@gmail.com>
2020-06-04 15:19:34 -0300

    #16

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,62 @@
 import React from "react";
 
-import { Button, OutlineButton } from "../../components/base/buttons";
+import {
+  Button,
+  OutlineButton,
+  LargeButton,
+  SquareSharpButton,
+  QtdInfoButton,
+  QtdInfoButtonSamll,
+  DiscountInfoButton,
+  LargeDiscountInfoButton,
+} from "../../components/base/buttons";
+
+import {
+  ShoppingCartICon,
+  PlusIcon,
+  SearchIcon,
+  BackArrowIcon,
+  MinusIcon,
+} from "../../components/base/icons";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <Button>Adicionar à Sacola</Button>
+      <Button>
+        <ShoppingCartICon />
+      </Button>
+      <Button>
+        <PlusIcon />
+      </Button>
+      <Button>
+        <MinusIcon />
+      </Button>
+      <Button>
+        <SearchIcon />
+      </Button>
+      <Button>
+        <BackArrowIcon />
+      </Button>
+      &nbsp;&nbsp;&nbsp;
+      <QtdInfoButton>+99</QtdInfoButton>&nbsp;&nbsp;&nbsp;
+      <QtdInfoButtonSamll>9</QtdInfoButtonSamll>&nbsp;&nbsp;&nbsp;
+      <DiscountInfoButton>99%</DiscountInfoButton>&nbsp;&nbsp;&nbsp;
+      <LargeDiscountInfoButton>99%</LargeDiscountInfoButton>&nbsp;&nbsp;&nbsp;
+      <SquareSharpButton>
+        <MinusIcon />
+      </SquareSharpButton>
+      &nbsp;&nbsp;&nbsp;
+      <SquareSharpButton>
+        <PlusIcon />
+      </SquareSharpButton>
+      <br />
+      <br />
+      <LargeButton>Large Button</LargeButton> <br />
+      <br />
       <OutlineButton>Remover da Sacola</OutlineButton>
+      <br />
+      <br />
     </div>
   );
 };
