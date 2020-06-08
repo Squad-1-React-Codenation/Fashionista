251ceb6ea74fc08b9fe2acbff42757a49406dfc6
Genilda Silva <63377949+GenildaSilva@users.noreply.github.com>
2020-06-08 19:59:45 -0300

    [issue 19] componente footer (#35)
    * [issue 19] componente footer
    
    * fix linter errors
    
    Co-authored-by: Herbert Vieira <herbert.vpb@gmail.com>

---
+++ src/components/base/iconsocial/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+type SocialIconsType = {
+  height: number;
+  width: number;
+};
+
+export const Instagram = ({ height, width }: SocialIconsType) => {
+  return (
+    <a
+      href="https://www.instagram.com/app_undefined/?hl=pt-br"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <img src="/insta.png" height={height} width={width} />
+    </a>
+  );
+};
+
+export const Facebook = ({ height, width }: SocialIconsType) => {
+  return <img src="/facebook.png" height={height} width={width} />;
+};
+
+export const Twiter = ({ height, width }: SocialIconsType) => {
+  return <img src="/black-twitter.png" height={height} width={width} />;
+};
