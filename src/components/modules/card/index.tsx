91998c4538d1ba23452366f10463d6ec8c8ec551
Genilda Silva <genildagomes.silva@gmail.com>
2020-06-20 09:45:39 -0300

    add label de desconto aos cards

--- src/components/modules/card/index.tsx
+++ src/components/modules/card/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { ProductType } from "../../../services/types";
 import imageNotFound from "../../../assets/image-not-found.png";
+import { DiscountInfoButton } from "../../base/buttons";
 
 export const Card = (product: ProductType) => {
   return (
     <div className="card">
       <img src={product.image ? product.image : imageNotFound} alt=""></img>
+      {product.on_sale ? (
+        <div className="card__discount-button">
+          <DiscountInfoButton>
+            {product.discount_percentage}
+          </DiscountInfoButton>
+        </div>       
+      )
+      : (<span></span>)}
       <div className="card__name"> {product.name} </div>
       {product.on_sale ? (
         <div className="card__price">
@@ -19,6 +28,7 @@ export const Card = (product: ProductType) => {
       ) : (
         <div className="card__regular-price">{product.regular_price}</div>
       )}
+      
     </div>
   );
 };
