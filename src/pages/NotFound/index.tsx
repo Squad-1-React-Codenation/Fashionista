19d90b60185a48781f15bd8010b73d3c4caa8c99
paulacresende <pcarvalhoresende@gmail.com>
2020-06-24 08:38:55 -0300

    adding public image path

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import pageNotFound from "../../assets/page-not-found.png";
 import { LargeButton } from "../../components/base/buttons";
 import { useHistory } from "react-router-dom";
 
@@ -11,7 +10,7 @@ export const NotFound = () => {
 
   return (
     <div className="container not-found">
-      <img src={pageNotFound} alt="" />
+      <img src="img/page-not-found.png" alt="" />
       <h1>Opss!</h1>
       <h3 className="not-found__text">{t("notFoundPage")}</h3>
       <LargeButton onClick={buttonHandler}>
