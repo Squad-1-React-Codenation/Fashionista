ee9c6d5996bfe1abd044bfd1db5480916aea2825
Herbert Vieira <herbert.vpb@gmail.com>
2020-06-24 22:44:59 -0300

    creating actions and reducers for the cart

--- src/store/cart/types.ts
+++ src/store/cart/types.ts
@@ -1,1 +1,28 @@
-export default true;
+import { ProductType } from "../../services/types";
+
+export enum CartAction {
+  ADD_TO_CART = "ADD_TO_CART",
+  REMOVE_FROM_CART = "REMOVE_FROM_CART",
+  CLEAR_CART = "CLEAR_CART",
+};
+
+export interface AddToCartAction {
+  type: CartAction.ADD_TO_CART;
+  product: ProductType;
+};
+
+export interface RemoveFromCartAction {
+  type: CartAction.REMOVE_FROM_CART;
+  id: string;
+};
+
+export interface ClearCartAction {
+  type: CartAction.CLEAR_CART;
+};
+
+export type CartStateType = {
+  count: number;
+  listing: ProductType[];
+};
+
+export type CartActionTypes = AddToCartAction | RemoveFromCartAction | ClearCartAction;
