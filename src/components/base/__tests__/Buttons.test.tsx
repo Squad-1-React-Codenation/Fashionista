afc0416bb25ea5faf186b66c99690c89984776a6
paulacresende <pcarvalhoresende@gmail.com>
2020-07-06 21:01:07 -0300

    testing buttons

--- src/components/base/__tests__/Buttons.test.tsx
+++ src/components/base/__tests__/Buttons.test.tsx
@@ -1,37 +1,48 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 
-import { Button, SquareSharpButton, LargeButton, SizeInfoButton } from "../buttons"
+import {
+  Button,
+  SquareSharpButton,
+  LargeButton,
+  SizeInfoButton,
+} from "../buttons";
 
 describe("The component <Button />", () => {
-    const mockButton = { children: "component" , disabled: true, color: "red", modifier: "large", sizes: "M" };
-    it("Should render Button component with children", async () => {
-        render(<Button children={mockButton.children}></Button>)
-        
-        expect(await screen.findByText("component")).toBeTruthy();
-    });
+  const mockButton = {
+    children: "component",
+    disabled: true,
+    color: "red",
+    modifier: "large",
+    sizes: "M",
+  };
+  it("Should render Button component with children", async () => {
+    render(<Button children={mockButton.children}></Button>);
+
+    expect(await screen.findByText("component")).toBeTruthy();
+  });
 });
 
 describe("The component <SquareSharpButton />", () => {
-    it("Should render SquareSharpButton component", async () => {
-        render(<SquareSharpButton />)
-        
-        expect(await screen.findByTestId("squareSharpButton")).toBeTruthy();
-    });
+  it("Should render SquareSharpButton component", async () => {
+    render(<SquareSharpButton />);
+
+    expect(await screen.findByTestId("squareSharpButton")).toBeTruthy();
+  });
 });
 
 describe("The component <SizeInfoButton />", () => {
-    it("Should render SizeInfoButton component", async () => {
-        render(<SizeInfoButton />)
-        
-        expect(await screen.findByTestId("sizeInfoButton")).toBeTruthy();
-    });
+  it("Should render SizeInfoButton component", async () => {
+    render(<SizeInfoButton />);
+
+    expect(await screen.findByTestId("sizeInfoButton")).toBeTruthy();
+  });
 });
 
 describe("The component <LargeButton />", () => {
-    it("Should render LargeButton component", async () => {
-        render(<LargeButton />)
-        
-        expect(await screen.findByTestId("largeButton")).toBeTruthy();
-    });
+  it("Should render LargeButton component", async () => {
+    render(<LargeButton />);
+
+    expect(await screen.findByTestId("largeButton")).toBeTruthy();
+  });
 });
