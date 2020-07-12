72d326870b51a5d923c82d2a9bd51713e170c96c
Genilda Silva <genildagomes.silva@gmail.com>
2020-07-12 17:36:40 -0300

    testes nos componentes do footer

--- src/components/base/iconSocial/IconsSocial.test.tsx
+++ src/components/base/iconSocial/IconsSocial.test.tsx
@@ -4,8 +4,9 @@ import "@testing-library/jest-dom";
 
 import { Instagram, Facebook, Twitter } from ".";
 
+const mockedProps = { height: 24, width: 24 };
+
 describe("The component IconSocial", () => {
-  const mockedProps = { height: 24, width: 24 };
 
   it("Should render Instagram component passing height and width", () => {
     render(<Instagram {...mockedProps} />);
@@ -22,3 +23,21 @@ describe("The component IconSocial", () => {
     expect(screen.getByAltText("Twitter")).toBeInTheDocument();
   });
 });
+
+describe("The component IconSocial will redirect", () => {
+  
+  it("Should redirect when clicking on Instagram anchor", () => {
+    render(<Instagram {...mockedProps} />);
+    expect(document.querySelector("a")?.getAttribute("href")).toBe("https://www.instagram.com/app_undefined/?hl=pt-br");
+  })
+
+  it("Should redirect when clicking on Facebook anchor", () => {
+    render(<Facebook {...mockedProps} />);
+    expect(document.querySelector("a")?.getAttribute("href")).toBe("https://www.facebook.com/");
+  })
+
+  it("Should redirect when clicking on Twitter anchor", () => {
+    render(<Twitter {...mockedProps} />);
+    expect(document.querySelector("a")?.getAttribute("href")).toBe("https://twitter.com/");
+  })
+})
