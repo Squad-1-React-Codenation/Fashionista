7b9e5008d344368bc441e24c39112c7a3fac3112
Genilda Silva <genildagomes.silva@gmail.com>
2020-07-12 17:42:17 -0300

    testes nos componentes do footer

--- src/components/base/iconSocial/IconsSocial.test.tsx
+++ src/components/base/iconSocial/IconsSocial.test.tsx
@@ -7,7 +7,6 @@ import { Instagram, Facebook, Twitter } from ".";
 const mockedProps = { height: 24, width: 24 };
 
 describe("The component IconSocial", () => {
-
   it("Should render Instagram component passing height and width", () => {
     render(<Instagram {...mockedProps} />);
     expect(screen.getByAltText("Instagram")).toBeInTheDocument();
@@ -28,16 +27,22 @@ describe("The component IconSocial will redirect", () => {
   
   it("Should redirect when clicking on Instagram anchor", () => {
     render(<Instagram {...mockedProps} />);
-    expect(document.querySelector("a")?.getAttribute("href")).toBe("https://www.instagram.com/app_undefined/?hl=pt-br");
-  })
+    expect(document.querySelector("a")?.getAttribute("href")).toBe(
+      "https://www.instagram.com/app_undefined/?hl=pt-br"
+    );
+  });
 
   it("Should redirect when clicking on Facebook anchor", () => {
     render(<Facebook {...mockedProps} />);
-    expect(document.querySelector("a")?.getAttribute("href")).toBe("https://www.facebook.com/");
-  })
+    expect(document.querySelector("a")?.getAttribute("href")).toBe(
+      "https://www.facebook.com/"
+    );
+  });
 
   it("Should redirect when clicking on Twitter anchor", () => {
     render(<Twitter {...mockedProps} />);
-    expect(document.querySelector("a")?.getAttribute("href")).toBe("https://twitter.com/");
-  })
-})
+    expect(document.querySelector("a")?.getAttribute("href")).toBe(
+      "https://twitter.com/"
+    );
+  });
+});
