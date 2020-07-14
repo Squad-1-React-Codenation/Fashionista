f2b23b85003f241667c371b8946ee5c9dd856af1
Genilda Silva <63377949+GenildaSilva@users.noreply.github.com>
2020-07-14 14:10:33 -0300

    testes nos componentes do footer (#152)
    * testes nos componentes do footer
    
    * testes nos componentes do footer
    
    * testes nos componentes do footer
    
    * teste footer e componentes
    
    * teste footer e componentes

--- src/components/base/iconSocial/IconsSocial.test.tsx
+++ src/components/base/iconSocial/IconsSocial.test.tsx
@@ -3,9 +3,9 @@ import { render, screen } from "@testing-library/react";
 
 import { Instagram, Facebook, Twitter } from ".";
 
-describe("The component IconSocial", () => {
-  const mockedProps = { height: 24, width: 24 };
+const mockedProps = { height: 24, width: 24 };
 
+describe("The component IconSocial", () => {
   it("Should render Instagram component passing height and width", () => {
     render(<Instagram {...mockedProps} />);
     expect(screen.getByAltText("Instagram")).toBeInTheDocument();
@@ -19,5 +19,22 @@ describe("The component IconSocial", () => {
   it("Should render Twitter component passing height and width", () => {
     render(<Twitter {...mockedProps} />);
     expect(screen.getByAltText("Twitter")).toBeInTheDocument();
+  });
+});
+
+describe("The component IconSocial will redirect", () => {
+  it("Should redirect when clicking on Instagram anchor", () => {
+    render(<Instagram {...mockedProps} />);
+    expect(screen.getByRole("link", { name: "Instagram" }));
+  });
+
+  it("Should redirect when clicking on Facebook anchor", () => {
+    render(<Facebook {...mockedProps} />);
+    expect(screen.getByRole("link", { name: "Facebook" }));
+  });
+
+  it("Should redirect when clicking on Twitter anchor", () => {
+    render(<Twitter {...mockedProps} />);
+    expect(screen.getByRole("link", { name: "Twitter" }));
   });
 });
