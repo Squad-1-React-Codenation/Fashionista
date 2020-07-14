aec20f3d7c9a0a69a91ab29b988404185fda6647
Marcelo Reis <oi@marcelreis.dev>
2020-07-14 08:32:35 -0300

    jest custom coverage

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,5 +5,7 @@ import "react-i18next";
 
 jest.mock("react-i18next", () => ({
   ...jest.requireActual("react-i18next"),
+  // This returns the same string, not using translation, for the sake of simplicity on our tests
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   useTranslation: () => ({ t: (s) => s }),
 }));
